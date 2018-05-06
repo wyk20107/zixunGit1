8f69bddc5dd31f85684929db670112ccf56a61f6
wyk20107 <497468450@qq.com>
2018-05-06 20:32:32 +0800

    Revert "Initial commit"
    This reverts commit 3d6b8b826bad2fc8c96f6cc423ec869b53b5295c.

--- src/main/java/com/wyh/zixun/async/EventHandler.java
+++
@@ -1,8 +0,0 @@
-package com.wyh.zixun.async;
-
-import java.util.List;
-
-public interface EventHandler {
-    void doHandle(EventModel model);
-    List<EventType> getSupportEventTypes();
-}
--- src/main/java/com/wyh/zixun/model/EntityType.java
+++
@@ -1,5 +0,0 @@
-package com.wyh.zixun.model;
-
-public class EntityType {
-    public static String ENTITY_NEWS = "NEWS";
-}
